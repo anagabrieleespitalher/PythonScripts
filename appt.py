883007485358f5e6aa94c2cb6258ce1f26b31e3c
Ana Gabriele Espitalher <109756604+aninhadasquebradas@users.noreply.github.com>
2023-04-03 19:24:52 -0300

    If

---
+++ appt.py
@@ -0,0 +1,11 @@
+
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número'))
+
+if num1 == num2:
+    print(f'{num1} e {num2} são IGUAIS')
+
+elif num1 < num2:
+    print(f' O {num2} é maior')
+else:
+    print(f'{} e {} não são iguais')
